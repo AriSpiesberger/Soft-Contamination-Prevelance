d25dcb0c358cb9854442393be3cd4f80782ac767
nicky p <work@nicky.pro>
2026-01-06 22:24:25 +0000

    simple scripts for multiple finetunes

--- finetuning/ft_zebra.sh
+++ finetuning/ft_zebra.sh
@@ -11,5 +11,5 @@ zebralogic-sd-shuffle_and_paraphrase-shards-000-to-009-of-010-ver1.jsonl
 
 for ft_file_name in ${ft_files[@]}; do
     full_path=$"./datasets/teacher_answers/zebralogic/$ft_file_name"
-    python3 p2_finetune_model.py -a $full_path -o "outputs/checkpoints/olmo3-qlora-{wandb_id}" -e 5
+    python3 p2_finetune_model.py -a $full_path -o "outputs/checkpoints/olmo3-qlora-{wandb_id}" -e 10
 done
