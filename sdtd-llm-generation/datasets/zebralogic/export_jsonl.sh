f1b14a2718cc65ef9edf5c0dc3e094199da7120d
Tomas Gavenciak <gavento@gmail.com>
2026-01-11 09:42:26 +0000

    Update the scripts to use reasoning

--- sdtd-llm-generation/datasets/zebralogic/export_jsonl.sh
+++ sdtd-llm-generation/datasets/zebralogic/export_jsonl.sh
@@ -3,21 +3,22 @@ set -e
 cd "$(dirname "$0")"
 
 SD_VARIANTS="paraphrase shuffle_and_paraphrase shuffle_and_substitute shuffle_and_substitute_and_paraphrase"
-VER=ver1
+VER=ver2 # Reasoning version
+TGT=jsonl-with-reasoning
 
-mkdir -p jsonl
+mkdir -p $TGT
 
 echo "=== Processing originals ==="
 FN="zebralogic-original"
 for i in 0 1 2 3 4 5 6 7 8 9 ; do
     FN2="${FN}-shard-00${i}-of-010"
     set -x
-    ../../sdtd-cli export-jsonl -t zebralogic -i "original/zebralogic-shard-00${i}-of-010.parquet" -o jsonl/$FN2-$VER.jsonl --sort-by-id-hash --debug
+    ../../sdtd-cli export-jsonl -t zebralogic -i "original-enriched-reasoning/zebralogic-shard-00${i}-of-010.parquet" -o $TGT/$FN2-$VER.jsonl --sort-by-id-hash --debug
     set +x
 done
 set -x
-cat jsonl/$FN-shard-00[01234]-of-010-$VER.jsonl > jsonl/$FN-shards-000-to-004-of-010-$VER.jsonl
-cat jsonl/$FN-shard-00[56789]-of-010-$VER.jsonl > jsonl/$FN-shards-005-to-009-of-010-$VER.jsonl
+cat $TGT/$FN-shard-00[01234]-of-010-$VER.jsonl > $TGT/$FN-shards-000-to-004-of-010-$VER.jsonl
+cat $TGT/$FN-shard-00[56789]-of-010-$VER.jsonl > $TGT/$FN-shards-005-to-009-of-010-$VER.jsonl
 set +x
 
 for base in $SD_VARIANTS; do
@@ -26,11 +27,11 @@ for base in $SD_VARIANTS; do
     for i in 0 1 2 3 4 5 6 7 8 9 ; do
         FN2="${FN}-shard-00${i}-of-010"
         set -x
-        ../../sdtd-cli export-jsonl -t parquet -i sds/$FN2.parquet -o jsonl/$FN2-$VER.jsonl --sort-by-id-hash --debug
+        ../../sdtd-cli export-jsonl -t parquet -i sds-enriched-reasoning/$FN2.parquet -o $TGT/$FN2-$VER.jsonl --sort-by-id-hash --debug
         set +x
     done
     set -x
-    cat jsonl/$FN-shard-00[01234]-of-010-$VER.jsonl > jsonl/$FN-shards-000-to-004-of-010-$VER.jsonl
-    cat jsonl/$FN-shard-00[56789]-of-010-$VER.jsonl > jsonl/$FN-shards-005-to-009-of-010-$VER.jsonl
+    cat $TGT/$FN-shard-00[01234]-of-010-$VER.jsonl > $TGT/$FN-shards-000-to-004-of-010-$VER.jsonl
+    cat $TGT/$FN-shard-00[56789]-of-010-$VER.jsonl > $TGT/$FN-shards-005-to-009-of-010-$VER.jsonl
     set +x
 done
--- sdtd-llm-generation/datasets/zebralogic/generate_sds.sh
+++ sdtd-llm-generation/datasets/zebralogic/generate_sds.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 
 SD_VARIANTS="paraphrase shuffle_and_paraphrase shuffle_and_substitute shuffle_and_substitute_and_paraphrase"
 # VER=ver1
-PARAMS="-n 5 --workers 16"
-TARGET=sds2
+PARAMS="-n 1000 --workers 4"
+TARGET=sds-enriched-reasoning
 
 mkdir -p $TARGET
 
@@ -14,6 +14,6 @@ for base in $SD_VARIANTS; do
     FN="zebralogic-sd-${base}"
     for i in 0 1 2 3 4 5 6 7 8 9 ; do
         FN2="${FN}-shard-00${i}-of-010"
-        ../../sdtd-cli generate -d zebralogic $PARAMS -l $base -i original/zebralogic-shard-00$i-of-010.parquet -o $TARGET/$FN2.parquet
+        ../../sdtd-cli generate -d zebralogic $PARAMS -l $base -i original-enriched-reasoning/zebralogic-shard-00$i-of-010.parquet -o $TARGET/$FN2.parquet
     done
 done
